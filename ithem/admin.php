71d3582cbfcfff90735fc4e8597b29bb6d5d52d2
Lambia <luca@lambia.it>
2016-10-09 21:14:57 +0200

    Backend
    Improving panel

--- ithem/admin.php
+++ ithem/admin.php
@@ -22,50 +22,61 @@ $core = new ithem($_SERVER['PHP_SELF']);
 
 $list = $core->get_list(null, null, null, "*", "rel ASC");
 
-foreach ($list as $item) {
-?>
-
-<input type="text" disabled maxlength="4" size="4" style="text-align:center;" value="<?=$item['id']?>" />
-<input type="text" value="<?=$item['name']?>" />
-<input type="text" value="<?=$item['value']?>" />
-<select>
-<?
+echo "<input type='text' maxlength='4' size='4' style='text-align:center;' value='id' />\n<input type='text' value='name' />\n<input type='text' value='value' />\n<select>\n\t<option selected value='type'>type</option>";
 $types = array_unique(array_column($list, 'type'));
+                          
 foreach($types as $type) {
-    echo "<option value='$type'";
+    echo "\t<option value='$type'>".$type."</option>\n";
+}
+
+echo "</select>\n<select>\n\t<option selected value='rel'>rel</option>";
+
+$rels = array_unique(array_column($list, 'rel'));
+foreach($rels as $rel) {
+    echo "\t<option value='$rel'>".$rel."</option>\n";
+}
+echo "</select>\n<select>\n\t<option selected value='children'>child</option>";
+
+$children = array_unique(array_column($list, 'children'));
+foreach($children as $child) {
+    echo "\t<option value='$child'>".$child."</option>\n";
+}
+echo "</select>\n<br/>\n\n";
+
+foreach ($list as $item) {
+    
+echo "<input type='text' disabled maxlength='4' size='4' style='text-align:center;' value='".$item['id']."' />\n<input type='text' value='".$item['name']."' />\n<input type='text' value='".$item['value']."' />\n<select>\n\t<option selected value='type' disabled>type</option>\n";
+    
+$types = array_unique(array_column($list, 'type'));
+                          
+foreach($types as $type) {
+    echo "\t<option value='$type'";
     if($type == $item['type']) {
         echo "selected='selected'";
     }
-    echo ">".$type."</option>";
+    echo ">".$type."</option>\n";
 }
-?>
-</select>
-<select>
-<?
+    
+echo "</select>\n<select>\n\t<option selected value='rel' disabled>rel</option>";
+
 $rels = array_unique(array_column($list, 'rel'));
 foreach($rels as $rel) {
-    echo "<option value='$rel'";
+    echo "\t<option value='$rel'";
     if($rel == $item['rel']) {
         echo "selected='selected'";
     }
-    echo ">".$rel."</option>";
+    echo ">".$rel."</option>\n";
 }
-?>
-</select>
-<select>
-<?
+echo "</select>\n<select>\n\t<option selected value='children' disabled>child</option>";
+
 $children = array_unique(array_column($list, 'children'));
 foreach($children as $child) {
-    echo "<option value='$child'";
+    echo "\t<option value='$child'";
     if($child == $item['children']) {
         echo "selected='selected'";
     }
-    echo ">".$child."</option>";
+    echo ">".$child."</option>\n";
 }
-?>
-</select>
-<br/>
-
-<?
+echo "</select>\n<br/>\n\n";
 }
 ?>
